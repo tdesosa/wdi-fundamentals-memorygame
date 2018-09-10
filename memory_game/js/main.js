823a10d49758d8897423a1d1f4b068548c56b214
Tomas DeSosa <TDeSosa@TD-MacAir.local>
2018-09-09 18:16:48 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -35,7 +35,9 @@ var checkForMatch = function () {
 	}
 }
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+
+	var cardId = this.getAttribute("data-id");
 
 	console.log("User flipped " + cards[cardId].rank);
 
@@ -44,10 +46,21 @@ var flipCard = function (cardId) {
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 
+	this.setAttribute("src", cards[cardId].cardImage);
+
 	checkForMatch();
 
-}
+};
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+};
+
+createBoard();
 
